ba3440b569fc6cd40fb87d75fc8fc28f435e91a6
Joebye <evgenii.prs@gmail.com>
2023-10-22 17:41:51 +0300

    pulse-probes imitator

---
+++ src/main/java/telran/monitoring/PulseProbesImitatorApplication.java
@@ -0,0 +1,39 @@
+package telran.monitoring;
+
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.monitoring.dto.PulseProbe;
+import telran.monitoring.service.PulseProbesImitator;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class PulseProbesImitatorApplication {
+private static final long TIMEOUT = 30000;
+final PulseProbesImitator imitator;
+	public static void main(String[] args) throws InterruptedException {
+		ConfigurableApplicationContext ctx = SpringApplication.run(PulseProbesImitatorApplication.class, args);
+		Thread.sleep(TIMEOUT);
+		ctx.close();
+		
+	}
+	
+	
+	@Bean
+	Supplier<PulseProbe> probesSupplier() {
+		return () -> {
+			PulseProbe probe = imitator.nextProbe();
+			log.trace("probe: {}", probe);
+			return probe;
+		};
+	}
+
+}
---
+++ src/main/java/telran/monitoring/service/PulseProbesImitatorImpl.java
@@ -0,0 +1,111 @@
+package telran.monitoring.service;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.text.ChangedCharSetException;
+
+import org.bouncycastle.oer.its.ieee1609dot2.basetypes.ThreeDLocation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import telran.monitoring.dto.PulseProbe;
+
+//app.probes.imitator.value.min=40
+//app.probes.imitator.value.max=220
+//app.probes.imitator.jump.prob=10
+//app.probes.imitator.jump.multiplier=0.4
+//app.probes.imitator.nojump.max.multiplier=0.05
+//app.probes.imitator.jump.increase.prob=70
+//app.probes.imitator.patients.amount=11
+
+
+@Service
+@Slf4j
+public class PulseProbesImitatorImpl implements PulseProbesImitator {
+	@Value("${app.probes.imitator.value.min}")
+	int minValue;
+	
+	@Value("${app.probes.imitator.value.max}")
+	int maxValue;
+	
+	@Value("${app.probes.imitator.jump.prob}")
+	int jumpProbability;
+	
+	@Value("${app.probes.imitator.jump.multiplier}")
+	double jumpMultiplier;
+	
+	@Value("${app.probes.imitator.nojump.max.multiplier}")
+	double noJumpMaxMultiplier;
+
+	@Value("${app.probes.imitator.jump.increase.prob}")
+	int increaseProbability;
+	
+	@Value("${app.probes.imitator.patients.amount}")
+	int nPatients;
+	
+	HashMap<Long, Integer> valuesMap = new HashMap<>();
+	int seqNumber = 0;
+	
+	@Override
+	public PulseProbe nextProbe() {
+		long patientId = getPatientId();
+		int value = getAndUpdateValue(patientId);
+		return new PulseProbe(patientId, value, System.currentTimeMillis(), ++seqNumber);
+	}
+
+	private int getAndUpdateValue(long patientId) {
+		int oldValue = valuesMap.getOrDefault(patientId, 0);
+		int newValue = oldValue == 0 ? (int) getRandomLong(minValue, maxValue) :
+			getNewValue(oldValue);
+		valuesMap.put(patientId,newValue);
+		return newValue;
+		
+	}
+
+	private int getNewValue(int oldValue) {
+		boolean isJump = chance(jumpProbability);
+		int newValue = isJump ? jumpValue(oldValue) : noJumpValue(oldValue);
+		log.trace("old value: {}, new Value: {}, isJump: {}", oldValue, newValue, isJump);
+		return newValue;
+	}
+
+	private int noJumpValue(int oldValue) {
+		double delta = oldValue * getDoubleRandom(0.0, noJumpMaxMultiplier);
+		int res = getValueDelta(oldValue, delta);
+		return res;
+	}
+
+	private double getDoubleRandom(double min, double max) {
+		return ThreadLocalRandom.current().nextDouble(min, max);
+	}
+
+	private int jumpValue(int oldValue) {
+		double delta = oldValue * jumpMultiplier;
+		int res = getValueDelta(oldValue, delta);
+		return res;
+	}
+
+	private int getValueDelta(int oldValue, double delta) {
+		boolean isIncrease = chance(increaseProbability);
+		if (!isIncrease) {
+			delta = -delta;
+		}
+		return (int) Math.round(oldValue + delta);
+	}
+
+	private boolean chance(int probability) {
+		
+		return getRandomLong(0, 100) < probability;
+	}
+
+	private long getPatientId() {
+		return getRandomLong(1, nPatients + 1);
+	}
+
+	private long getRandomLong(int min, int max) {
+		return ThreadLocalRandom.current().nextLong(min, max);
+	}
+
+}
